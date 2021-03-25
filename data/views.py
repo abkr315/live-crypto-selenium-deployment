0f8636580fb4dcdb77cdfb5013fa930bae5fc072
M_Abubakar <17-cs-40@students.uettaxila.edu.pk>
2021-03-26 00:10:00 +0500

    driver executable path commit3

--- data/views.py
+++ data/views.py
@@ -3,15 +3,18 @@ from django.shortcuts import render
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from shutil import which
+import os
 
 # Create your views here.
 
 def index(request):
 
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    chrome_path = "C:\\Users\\MUHAMMADABUBAKAR\\best_movies\\chromedriver.exe"
-    driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     driver.set_window_size(1920, 1080)
     driver.get("https://www.skrill.com/en/crypto/live-cryptocurrency-prices/")
     #html = driver.page_source
